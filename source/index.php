27d80890661db9563e18230bd047a7864317da04
Rafael Birkmann <rafkafu@yahoo.de>
2015-01-21 12:49:27 +0100

    Changed db-connect

--- source/index.php
+++ source/index.php
@@ -1,5 +1,10 @@
 <?php
 
+	$username = "";
+	$password = "";
+	$database = "";
+	mysql_connect("localhost",$username,$password);
+	mysql_select_db($database) or die( "Unable to select database.");
 
 ?>
 
